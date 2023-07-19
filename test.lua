55d227dc1d0eaa8cfbf526c0a94d315e797510fd
Koma | Developer <77354592+tabysi@users.noreply.github.com>
2023-07-19 17:34:51 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,5 +1,5 @@
 function Test()
-    print("Das ist ein Test um zu Wissen ob Funktionen auch mitgegeben werden <3")
+    print("Das ist ein Test um zu Wissen ob Funktionen auch mitgegeben werden")
 end
 
 Test()
